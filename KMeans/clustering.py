c70a5235276c5d3624101d849222bb5a79b3d34d
Lukas Muttenthaler <LukasMut@users.noreply.github.com>
2019-09-22 10:35:12 +0200

    Updated k-Means algorithm, and added k-Medoids

--- KMeans/clustering.py
+++ KMeans/clustering.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 import random 
 
 
-def normalize_data(df, scaling:str):
+def data_normalization(df, scaling:str):
     """
-        Args: pd.DatFrame, feature scaling method (see below which are possible to choose from)
+        Args: pd.DataFrame, feature scaling method (see below which scaling methods are implemented)
         Return: normalized data set (converted into NumPy matrix)
     """
     scalings = ['standardize', 'min-max', 'mean-norm']
     if scaling not in scalings:
-        raise Exception('Features can only be scaled according to standardization, min-max or mean-norm scaling')
+        raise ValueError('Scaling method must be one of {standardization, min-max or mean-norm scaling}')
     for column in df.columns:
         if scaling == 'standardize':
             mean, std = df[column].values.mean(), df[column].values.std()
@@ -32,25 +32,24 @@ def normalize_data(df, scaling:str):
 
 class KMeans:
     
-    def __init__(self, init:str):
-        if init == 'random':
-            self.init = init
+    def __init__(self, random_state:int, method='k-means'):
+        self.random_state = random_state
+        methods = ['k-means', 'k-medoids']
+        if method in methods:
+            self.method = method
         else:
-            raise Exception('Means have to be initialised with randomly chosen data points')
+            raise ValueError("Invalid value for 'method': must be one of {k-means, k-medoids}")
             
     def init_means(self, data):
         """            
             Args: object and dataset
-            Return: two randomly chosen data points or two points at zero
+            Return: two randomly chosen data points that are a subset of D
             
-            Intialize the means by randomly taking two points from the dataset
-            or initialise them with zeros (investigate which method more rapidly leads to optimal means)
+            Intialize the means (or medians) by randomly taking two points from the dataset (both for k-Means or k-Medoids)
         """
         N = data.shape[0]
-        if self.init=='random':
-            idx_mean_0, idx_mean_1 = random.randint(0, N-1), random.randint(0, N-1)
-        else:
-            raise Exception('Initialise means with randomly chosen data points')
+        random.seed(self.random_state) # pick a fixed random seed to make experiment reproducible 
+        idx_mean_0, idx_mean_1 = random.randint(0, N-1), random.randint(0, N-1)
         return data[idx_mean_0, :2], data[idx_mean_1, :2]
     
     def euclidean_dist(self, data, mean_0, mean_1):
@@ -62,8 +61,10 @@ class KMeans:
             Update classes accordingly
         """
         means = [mean_0, mean_1]
+        # objective function: l2-norm (euclidean distance) for k-Means; l1-norm (absolut error criterion) for k-Medoids
+        norm = 2 if self.method == 'k-means' else 1
         # fastest way, I am aware of, to update classes of all data points simultaneously (maybe there is a faster way? let me know :) )
-        data[:, 2] = [np.argmin([np.linalg.norm(point-mean) for mean in means]) for point in data[:, :2]]
+        data[:, 2] = [np.argmin([np.linalg.norm(point-mean, norm) for mean in means]) for point in data[:, :2]]
         return data
     
     def update_means(self, data, k=2): 
@@ -71,7 +72,10 @@ class KMeans:
             Args: dataset and number of classes
             Return: updated mean values per class
         """
-        return (np.mean(data[data[:, 2] == i][:, :2], axis=0) for i in range(k))
+        if self.method=='k-means':
+            return (np.mean(data[data[:, 2] == i][:, :2], axis=0) for i in range(k))
+        elif self.method=='k-medoids':
+            return (np.median(data[data[:, 2] == i][:, :2], axis=0) for i in range(k))
     
     def fit_predict(self, data, epochs):
         """
@@ -79,33 +83,34 @@ class KMeans:
             Return: dataset and optimized mean values for each of the two classes
         """
         mean_0, mean_1 = self.init_means(data)
+        #iterative relocation loop
         for epoch in range(epochs):
-            # create data copy to keep track of moving points (NumPy computes changes in place)
-            data_copy = np.copy(data)
+            # create data copy to keep track of moving points (NumPy computes changes in place - thus, we need to create a copy of D)
+            data_previous = np.copy(data)
             # compute distance between each point and the two mean values, and assign classes
             data = self.euclidean_dist(data, mean_0, mean_1)
-            plot_centroids(data, mean_0, mean_1)
-            if np.all(data[:, 2] == data_copy[:, 2]):
-                print('0 points moved this round. We could stop optimization, but lets also compare the centroids first.')
-            else:
-                booleans = data[:, 2] == data_copy[:, 2]
-                n_points_moved = len(booleans[booleans==False])
-                string = 'points' if n_points_moved > 1 else 'point'
-                print('{} {} moved this round. Optimization continues.'.format(n_points_moved, string))
-            mean_0_current, mean_1_current = self.update_means(data)
-            # if mean values have not changed, then break loop and stop optimization
-            if np.all(mean_0_current == mean_0) and np.all(mean_1_current == mean_1):
-                print('Algorithm needed {} iterations until optimal mean values were found.'.format(epoch+1))
+            algorithm_label = r'(k-Means)' if self.method == 'k-means' else r'(k-Medoids)'
+            centroid_label = r'$\bar x' if self.method == 'k-means' else r'$\tilde x'
+            plot_clusters(data, mean_0, mean_1, algorithm_label, centroid_label)
+            if np.all(data[:, 2] == data_previous[:, 2]):
+                 # if no data point moved this round (i.e., centroids did not change), then break loop and stop optimization
+                print('0 points moved this round. Optimization will stop.')
+                print()
+                print('Algorithm needed {} iterations until convergence.'.format(epoch+1))
                 break
             else:
-                mean_0, mean_1 = mean_0_current, mean_1_current
+                booleans = data[:, 2] == data_previous[:, 2]
+                n_points_moved = len(booleans[booleans==False])
+                p_string = 'points' if n_points_moved > 1 else 'point'
+                print('{} {} moved this round. Optimization continues.'.format(n_points_moved, p_string))
+            mean_0, mean_1 = self.update_means(data)
         return data, mean_0, mean_1
 
-def plot_centroids(data, mean_0, mean_1):    
-    plt.title('Data distribution and corresponding centroids')
-    plt.scatter(data[data[:, 2] == 0][:,0], data[data[:, 2] == 0][:, 1], color = 'green', label='0')
-    plt.scatter(data[data[:, 2] == 1][:,0], data[data[:, 2] == 1][:, 1], color = 'blue', label='1')
-    plt.scatter(mean_0[0], mean_0[1], color = 'red', label=r'$\bar x_0$')
-    plt.scatter(mean_1[0], mean_1[1], color = 'orange', label=r'$\bar x_1$')
+def plot_clusters(data, mean_0, mean_1, algorithm_label, centroid_label):    
+    plt.title('Clusters and corresponding centroids' + ' ' + algorithm_label)
+    plt.scatter(data[data[:, 2] == 0][:,0], data[data[:, 2] == 0][:, 1], color = 'turquoise', label='0')
+    plt.scatter(data[data[:, 2] == 1][:,0], data[data[:, 2] == 1][:, 1], color = 'cornflowerblue', label='1')
+    plt.scatter(mean_0[0], mean_0[1], color = 'red', label = centroid_label + '_0$')
+    plt.scatter(mean_1[0], mean_1[1], color = 'darkorange', label = centroid_label + '_1$')
     plt.legend(fancybox=True, framealpha=1, loc='lower right', prop={'size':10})
     plt.show()
